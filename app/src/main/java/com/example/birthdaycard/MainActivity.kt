933b14d207bbb01b8e9e8e562d81f9e690f228bd
adityaliberty <adityaliberty33@gmail.com>
2021-02-22 15:47:46 +0530

    initial commit

---
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.birthdaycard
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun createCard(view: View) {
+        val name = nameInput.editableText.toString()
+
+        val intent=Intent(this, GreetingActivity::class.java)
+        intent.putExtra(GreetingActivity.NAME_EXTRA, name)
+        startActivity(intent)
+
+
+
+       //Toast.makeText(this, "Loading......", Toast.LENGTH_LONG).show()
+    }
+}
